63a762053b147fb5292764fe10d254b56851ea4a
nicks1555 <nicks1555@gmail.com>
2023-05-27 14:01:56 +0530

    updated maven prg

--- src/test/java/gmaillogin/Login.java
+++ src/test/java/gmaillogin/Login.java
@@ -5,13 +5,13 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Login {
-
-	public static void main(String[] args) {
-
+		@Test
+		void main1() {
 		WebDriverManager.firefoxdriver().setup();
 		WebDriver driver=new FirefoxDriver();
 		driver.get("https://www.google.com/");
@@ -19,7 +19,5 @@ public class Login {
 		driver.findElement(By.xpath("//a[text()='Gmail']")).click();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.close();	
-
 	}
-
 }
